b1ecb64f8df4c420dc7657153ddee21efa7464e6
angeland-ou <angeland@outlook.com>
2026-04-28 12:11:45 +0200

    feat: add file upload in create & update movements routes

--- src/modules/movements/movements.routes.js
+++ src/modules/movements/movements.routes.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 const auth = require("../../middlewares/auth.middleware");
 const tenant = require("../../middlewares/tenant.middleware");
 const { createMovementController, getAllMovementsController, getMovementByIdController, updateMovementController, deleteMovementController } = require("./movements.controller");
+const upload = require("../../config/multer");
 
 // Rutas protegidas
 router.use(auth, tenant);
 
-router.post("/", createMovementController);
+router.post("/", upload.single("file"), createMovementController);
+router.put("/:id", upload.single("file"), updateMovementController);
+
 router.get("/", getAllMovementsController); // soporta ?projectId=
 router.get("/:id", getMovementByIdController);
-router.put("/:id", updateMovementController);
 router.delete("/:id", deleteMovementController);
 
 module.exports = router;
