89363a19c8cf7f652887bfacf3c007a1fac92f51
angeland-ou <angeland@outlook.com>
2026-04-28 11:55:40 +0200

    fix: auth middleware - error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,12 @@
 const { verifyAccessToken } = require("../utils/jwt");
 const { prisma } = require("../config/db");
+const { CError, ErrorsIndex } = require("../config/misc/errors");
 
 const authHandler = async (req, res, next) => {
     try {
         const accessToken = req.cookies.accessToken;
 
-        if(!accessToken) {
-            return res.status(401).json({
-                error: "No existe la cookie"
-            })
-        }
+        if(!accessToken) throw new CError(ErrorsIndex.UNAUTHORIZED, "Token inválido");
 
         const tokenVerify = verifyAccessToken(accessToken);
 
@@ -19,25 +16,9 @@ const authHandler = async (req, res, next) => {
             }
         })
 
-        if(!user){
-            return res.status(401).json({
-                error: "El usuario no existe"
-            })
-        }
-
-        const isActive = user.isActive;
-
-        if(!isActive){
-            return res.status(401).json({
-                error: "El usuario no está activo"
-            })
-        }
-
-        if(tokenVerify.tokenVersion !== user.tokenVersion){
-            return res.status(401).json({
-                error: "El token ha expirado"
-            })
-        }
+        if(!user) throw new CError(ErrorsIndex.UNAUTHORIZED);
+        if(!user.isActive) throw new CError(ErrorsIndex.UNAUTHORIZED);
+        if(tokenVerify.tokenVersion !== user.tokenVersion) throw new CError(ErrorsIndex.UNAUTHORIZED);
 
         req.user = {
             userId: user.id,
@@ -49,7 +30,7 @@ const authHandler = async (req, res, next) => {
         next();
 
     } catch (error) {
-        next(error)
+        next(error instanceof CError ? error : new CError(ErrorsIndex.UNAUTHORIZED));
     }
 }
 
